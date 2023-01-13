58282236015d0c299351eedbaed1e1ef0c15a38b
filipetenedini <tenedinifilipe@gmail.com>
2023-01-12 23:55:12 -0300

    Verify function add

--- assets/js/data/insertData.js
+++ assets/js/data/insertData.js
@@ -1,13 +1,12 @@
 import { imgLista } from './data.js';
 
-
 function startApp(){
     let cardsQt = 0;
 
-    while (cardsQt < 4 || cardsQt > 14 || cardsQt % 2 !== 0){
+    while (cardsQt < 4 || cardsQt > 14 || cardsQt % 2 !== 0){ //adicionar verificação para ver se é NaN ou não e separar por verificação
         cardsQt = prompt('Com quantas cartas você quer jogar? Pode escolher números pares de 4 a 14 ');
     }
-    cardsQt = Number(cardsQt)
+    cardsQt = Number(cardsQt / 2);
 
     createData(cardsQt);
 }
@@ -19,9 +18,11 @@ function createData(nmr) {
         cards.push(imgLista[i]);
         cards.push(imgLista[i]);
     }
+    
+    cards.sort(comparador);
 
     insertData(cards);
-    flipFunction();
+    addFunctions();
 }
 
 function insertData(array){
@@ -41,15 +42,49 @@ function insertData(array){
     });
 }
 
-function flipFunction(){
+function addFunctions(){
     const cardList = document.querySelectorAll('.card');
 
     cardList.forEach((item)=>{
-        console.log(item);
         item.addEventListener('click', () => {
             item.classList.toggle('flipped-card');
         });
+        
+        item.addEventListener('click', ()=>{
+            verifyCard(item);
+        });
     });
+    
+}
+
+function verifyCard(card){
+    card.classList.add('equal-verify');
+    const comparedItems = document.querySelectorAll('.equal-verify');
+    const maxComparedItems = 2;
+
+    if (comparedItems.length === maxComparedItems){
+        let cardOne = comparedItems[0].querySelector('.card-back img').getAttribute('src');
+        let cardTwo = comparedItems[1].querySelector('.card-back img').getAttribute('src');
+
+        if (cardOne !== cardTwo){
+            setTimeout(()=>{
+                comparedItems.forEach((comparedItem)=>{
+                    comparedItem.classList.remove('flipped-card');
+                    comparedItem.classList.remove('equal-verify');
+                });
+            }, 1000)
+        } else {
+            comparedItems.forEach((comparedItem)=>{
+                comparedItem.classList.remove('flipped-card');
+                comparedItem.classList.add('ok-card');
+                comparedItem.classList.remove('equal-verify');
+            });
+        }
+    }       
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 
 startApp();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
 import {} from './data/insertData.js';
+
+// proximas atualizações
+
+// contador de tempo
+// contador de jogadas
